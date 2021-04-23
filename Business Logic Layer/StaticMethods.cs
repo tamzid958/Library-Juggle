f0c2bd9b3c39e937851e47f6e79658b2e0d63a48
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-04-24 01:06:42 +0600

    Data Access Class Created

--- Business Logic Layer/StaticMethods.cs
+++ Business Logic Layer/StaticMethods.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Cryptography;
 using System.Text;
-using static System.StringComparison;
 
 namespace Library_Juggle.Business_Logic_Layer
 {
---
+++ Data Access Layer/GenreDataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Juggle.Data_Access_Layer
+{
+    public class GenreDataAccess
+    {
+
+        private readonly LibraryJuggleContext _db;
+
+        public GenreDataAccess()
+        {
+            _db = new LibraryJuggleContext();
+        }
+    }
+}
